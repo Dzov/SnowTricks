234f8c91e4f9c5c6a1bd44f94330eb2fd24ad005
Dzov <amelie.haladjian@gmail.com>
2018-09-10 11:47:57 +0200

    feat(upload): remove duplicated image view, style image upload

--- public/js/addImageInput.js
+++ public/js/addImageInput.js
@@ -5,18 +5,20 @@ String.prototype.replaceAll = function (search, replacement) {
 
 var $imagesForm = document.querySelector('#trick_form_images').parentElement;
 $imagesForm.classList += ' d-flex flex-column';
-var $imageInputs = document.querySelector('#trick_form_images');
-var template = $imageInputs.getAttribute('data-prototype');
+var $imageFormInputs = document.querySelector('#trick_form_images');
+$imageFormInputs.classList += ' hidden d-flex flex-column align-items-center';
+$imageFormInputs.style.border = 'none';
+var template = $imageFormInputs.getAttribute('data-prototype');
 
 var $button = document.createElement('div');
 $button.innerText = 'Ajouter une image';
 $button.classList = 'btn btn-primary mt-3';
 $button.type = 'addImage';
 $button.addEventListener('click', function () {
+    $imageFormInputs.classList.remove('hidden');
     addInput(id++);
 });
 $imagesForm.appendChild($button);
-
 
 $imageEditionInputs = [];
 $regex = new RegExp('trick_form_images_[\\d]+$');
@@ -32,16 +34,14 @@ for (var j = 0; j < $imageEditionInputs.length; j++) {
     $imageEditionInputs[j].className = 'hidden';
 }
 
-
 id = $imageEditionInputs.length + 1;
 
-console.log(id);
 
 function addInput (id) {
     var newTemplate = template.replaceAll('__name__label__', 'Image').replaceAll('__name__', id).trim();
 
     var $div = document.createElement('div');
     $div.innerHTML = newTemplate;
-    $imageInputs.appendChild($div);
+    $imageFormInputs.appendChild($div);
 }
 
