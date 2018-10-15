dff371803a3b359c58f3c2a50e251485c91f1788
Dzov <amelie.haladjian@gmail.com>
2018-10-15 15:58:32 +0200

    refacto(*) clean code

--- src/Service/DeleteTrickVideos.php
+++ src/Service/DeleteTrickVideos.php
@@ -32,12 +32,10 @@ class DeleteTrickVideos
 
         $videos = $this->entityManager->getRepository(Video::class)->findBy(['id' => $deletedVideosIds]);
 
-        dump($videos);
         foreach ($videos as $video) {
             if (null !== $video) {
                 $trick->removeVideo($video);
                 $this->entityManager->remove($video);
-//                $this->entityManager->flush();
             }
         }
     }
