49546a29f1e7f926232ce6500f8d05f0bb2b7df0
Dzov <amelie.haladjian@gmail.com>
2018-10-29 17:53:38 +0100

    feat(registration): format email

--- src/Service/SendValidationEmail.php
+++ src/Service/SendValidationEmail.php
@@ -27,7 +27,21 @@ class SendValidationEmail
         $message = (new \Swift_Message('Votre inscription sur SnowTricks'))
             ->setFrom('amelie2360@gmail.com')
             ->setTo('amelie2360@gmail.com')
-            ->setBody($this->appUri . 'validate?u=' . $user->getId() . '&t=' . $token);
+            ->setBody(
+                '<html>' .
+                '<head></head>' .
+                '<body>' .
+                '<h1>Bienvenue dans la communauté SnowTricks !</h1>' .
+                '<p>Vous pourrez bientôt profiter pleinement de la plateforme et contribuer à votre tour ! Il vous suffit à présent de valider votre inscription.
+                    Pour se faire, veuillez cliquer sur le lien ci-dessous:</p>' .
+                '<a href="' . $this->appUri . 'validate?u=' . $user->getId() . '&t=' . $token . '">
+                    Valider mon inscription
+                 </a>' .
+                '</body>' .
+                '</html>'
+                ,
+                'text/html'
+            );
 
         $this->mailer->send($message);
     }
