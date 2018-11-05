8d847b356a1c9225f8a9005381e2a6eba969f6c2
Dzov <amelie.haladjian@gmail.com>
2018-11-05 14:26:42 +0100

    feat(db): change migration

--- src/Migrations/Version20181105132548.php
+++ src/Migrations/Version20181105132548.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class SnowTricks extends AbstractMigration
+final class Version20181105132548 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
